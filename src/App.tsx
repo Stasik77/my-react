cdb7b093ded7694ad87b2f9a051b27aa2603e548
stas <staz86@mail.ru>
2024-10-06 22:12:06 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from "./components/Accordion/Accordion";
+import {Raiting} from "./components/Raiting";  //<<< с Помощью AS  можено переименовать Компоненту{Raiting as R}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+            <PageTitle title={"This is App"}/>
+            <PageTitle title={"Users"}/>
+            Article1
+            <Raiting value={3} />
+            <Accordion title={"menu"} />
+            <Accordion title={"menu2"}/>
+            Article2
+            <Raiting value={0}/>
+            <Raiting value={1}/>
+            <Raiting value={2}/>
+            <Raiting value={3}/>
+            <Raiting value={4}/>
+            <Raiting value={5}/>
+
+        </>
+
+    );
 }
+
+
+function PageTitle(props: any) {
+    debugger
+    return (
+        <h2>{props.title}</h2>
+    )
+}
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let users = [
+    {
+        name: "12",
+        age: 302,
+        address: {
+            city: "spb",
+            country: "usa",
+
+        }
+    },
+    {
+        name: "13",
+        age: 32,
+        address: {
+            city: "spb",
+            country: "usa",
+        }
+    }
+
+]
+let b: Array<number | boolean | string>
+
+b = [3,25,62,]
+b =[true]
+b = [""]
+
+console.log(users[0].age)
