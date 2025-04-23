a7e1d27f4a6b85355c164ab52d6655022f6002ab
stas <staz86@mail.ru>
2025-04-23 22:21:24 +0300

    memo

---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,49 @@
+
+import React, {useMemo, useState} from 'react';
+
+export default {
+    title: 'useMemoReact',
+}
+
+export const Example1 = () =>{
+
+    const[a,setA]=useState<number>(5);
+    const[b,setB]=useState<number>(5);
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA =  useMemo(()=>{
+
+let tempResultA = 1
+        for(let i=1;i<=a;i++){
+            let fake=0;
+            while(fake<10000){
+                fake++;
+                const fakeValue =Math.random()
+            }
+            resultA=resultA*i;
+
+        }
+        return tempResultA;
+
+    },[a])
+
+
+  for(let i=1;i<=b;i++){
+        resultB=resultB*i;
+    }
+
+    return <>
+        <input value={a} onChange={(e)=>setA(Number(e.currentTarget.value))}/>
+        <input value={b} onChange={(e)=>{setB(+e.currentTarget.value)}}/>
+        <hr/>
+        <div>
+            result for a:{resultA}
+        </div>
+        <div>
+            result for b:{resultB}
+        </div>
+    </>
+
+}
